16933456eefc4b7eed236ca53ae3b13b79b9b59c
torgish <torgish393@gmail.com>
2022-08-07 16:47:04 +0300

    add function 'add new post'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 import './styles/App.css'
 
@@ -12,8 +13,30 @@ function App() {
 		{id: 3, title: 'C++', body: 'Description'}
 	])
 
+	const [post, setPost] = useState({title: '', body: ''})
+
+	const addNewPost = (e) => {
+		e.preventDefault()
+
+		setPosts([...posts, {...post, id: Date.now()}])
+		setPost({title: '', body: ''})
+	}
+
 	return (
 		<div className="App">
+			<form>
+				<MyInput 
+					value={post.title}
+					onChange={e => setPost({...post, title: e.target.value})}
+					type="text"
+					placeholder="Название поста"/>
+				<MyInput 
+					value={post.body}
+					onChange={e => setPost({...post, body: e.target.value})}
+					type="text" 
+					placeholder="Описание поста"/>
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
+			</form>
 			<PostList posts={posts} title={'Список постов'}/>
 		</div>
 	);
